3e4ccd636266fa68966284ed597543f31a3898fc
Mustapha El Idrissi <appsbymuss@gmail.com>
2024-01-27 12:34:30 -0800

    Function print string

--- print_alphabets.c
+++ print_alphabets.c
@@ -12,3 +12,22 @@ int print_c(va_list list, flags_types *f)
 	_putchar(va_arg(list, int));
 	return (1);
 }
+
+/**
+ * print_s - iterate through strings charcters
+ * @list: variadic list
+ * @f: PTR to flags
+ * Return: len (str) printed
+ */
+int print_s(va_list list, flags_types *f)
+{
+	char *stri = va_arg(list, char *);
+	(void)f;
+
+	if (!(stri))
+	{
+		stri = "(null)";
+	}
+	return (_puts(stri));
+}
+
