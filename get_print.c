fd2b05202abbb3bc273324638a1d0120fcf4a722
Mustapha El Idrissi <appsbymuss@gmail.com>
2024-01-27 12:25:55 -0800

    Function Get print

---
+++ get_print.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int printt(va_list list, flags_types *f);
+
+/**
+ * get_print - fetch specific function by character
+ * @s: conversion char
+ * Description: loops the structs
+ * Return: PTR
+ */
+
+int (*get_print(char s))(va_list, flags_types *)
+{
+	printhand func_ar[] = {{'s', print_s}, {'c', print_c}};
+
+	int fgs = 14;
+
+	register int i;
+
+
+	for (i = 0; i < fgs; i++)
+	{
+		if (func_ar[i].c == s)
+		{
+			return (func_ar[i].f);
+		}
+	}
+
+	return (NULL);
+}
+
