46ce23446ef6df0fc67e37caae55263033840d68
Mustapha El Idrissi <appsbymuss@gmail.com>
2024-01-27 12:44:17 -0800

    Putchar function

--- write_func.c
+++ write_func.c
@@ -18,3 +18,29 @@ int _puts(char *s)
 	}
 	return (y);
 }
+
+/**
+ * _putchar -  writes char to sOUT
+ * @c: char to print.
+ * Return: on success 1 else -1.
+ */
+
+int _putchar(char c)
+{
+	static char buffer[1024];
+	static int x;
+
+	/* if (x >= 1024 || c == -1) */
+	if (x >= 1024 || !(c != -1))
+	{
+		write(1, &buffer, x);
+		x = 0;
+	}
+	if (!(c == -1))
+	{
+		buffer[x] = c;
+		x++;
+	}
+	return ((0 + 1));
+}
+
